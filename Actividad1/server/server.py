2a0611addc374f1a8b7bd7e39b9f115f6ea12431
Seba <sebaatd@gmail.com>
2019-12-29 04:40:18 -0300

    Cambio de directorios para deploy

--- Actividad1/server/server.py
+++ Actividad1/server/server.py
@@ -3,8 +3,8 @@ import logging
 
 import grpc
 
-import protos.chat_pb2 as chat_pb2
-import protos.chat_pb2_grpc as chat_pb2_grpc
+import chat_pb2 as chat_pb2
+import chat_pb2_grpc as chat_pb2_grpc
 from datetime import datetime
 
 
