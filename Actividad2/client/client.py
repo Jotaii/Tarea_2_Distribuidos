7aec6f767278c679da1538410177a367a06fee81
Seba <sebaatd@gmail.com>
2020-01-02 20:41:48 -0300

    Login/Register y send

--- Actividad2/client/client.py
+++ Actividad2/client/client.py
@@ -7,22 +7,124 @@ import pika
 import os
 import uuid
 import sys
+import json
+from datetime import datetime
+
 
 class Client:
 
     def __init__(self):
         print("Iniciando conexión con RabbitMQ, por favor espere...")
         #time.sleep(10)
-        self.username = input("Ingrese nombre de usuario: ")
-
-        # username logic
+        self.client_uuid = uuid.uuid4()
 
         self.connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
         self.channel = self.connection.channel()
-    
+
         self.channel.exchange_declare(exchange='user_channel', exchange_type='direct')
 
+    def register(self):
+        
+        self.cont = True
+        while self.cont:
+            result = self.channel.queue_declare(queue=str(self.client_uuid), exclusive=True)
+            queue_name = result.method.queue
+
+            self.channel.queue_bind(exchange='user_channel', queue=queue_name)
+
+            username = input("Ingrese un nombre de usuario: ")
+            password = input("Ingrese una contraseña: ")
+
+            message = {
+                'type': "register",
+                'username': username,
+                'password': password,
+                'uuid': str(self.client_uuid)
+                }
+
+            body_message = json.dumps(message)
+
+            self.channel.basic_publish(
+                exchange='',
+                routing_key='server_pending_messages',
+                body=body_message,
+                properties = pika.BasicProperties(content_type='text/plain', delivery_mode=2)
+            )
+
+            def callback(ch, method, properties, body):
+
+                if body.decode("utf-8") == "ok":
+                    self.username = username
+                    self.channel.basic_cancel(consumer_tag=str(self.client_uuid))
+                    self.channel.queue_delete(queue=queue_name)
+                    self.cont = False
+                    print("Se ha registrado correctamente")
+                
+                else:
+                    self.channel.basic_cancel(consumer_tag=str(self.client_uuid))
+                    self.channel.queue_delete(queue=queue_name)
+                    print("Usuario ya registrado")
+
+            self.channel.basic_consume(
+                queue=queue_name, on_message_callback=callback, auto_ack=True, consumer_tag=str(self.client_uuid))
+
+            self.channel.start_consuming()
+
+    def login(self):
+        self.cont = True
+
+        while self.cont:
+            result = self.channel.queue_declare(queue=str(self.client_uuid), exclusive=True)
+            queue_name = result.method.queue
+
+            self.channel.queue_bind(exchange='user_channel', queue=queue_name)
+            username = input("Ingrese un nombre de usuario: ")
+            password = input("Ingrese su contraseña: ")
+
+            now = datetime.now()
+            timestamp = datetime.timestamp(now)
+
+            request_json = {
+                'id': str(uuid.uuid4()),
+                'type': "login",
+                'username': username,
+                'password': password,
+                'uuid': str(self.client_uuid),
+                'timestamp': timestamp
+                }
+
+            request = json.dumps(request_json)
+
+            self.channel.basic_publish(
+                exchange='',
+                routing_key='server_pending_messages',
+                body=request,
+                properties = pika.BasicProperties(content_type='text/plain', delivery_mode=2)
+            )
+
+            def callback(ch, method, properties, body):
+                server_response_string = body.decode("utf-8")
+                server_response_json = json.loads(server_response_string)
+
+                if server_response_json["response"] == "ok":
+                    self.username = username
+                    self.channel.basic_cancel(consumer_tag=str(self.client_uuid))
+                    self.channel.queue_delete(queue=queue_name)
+                    self.cont = False
+                    print("Logueado correctamente")
+                
+                else:
+                    self.channel.basic_cancel(consumer_tag=str(self.client_uuid))
+                    self.channel.queue_delete(queue=queue_name)
+                    print("Credenciales inválidas o usuario ya logueado")
+
+            self.channel.basic_consume(
+                queue=queue_name, on_message_callback=callback, auto_ack=True, consumer_tag=str(self.client_uuid))
+
+            self.channel.start_consuming()
+
     def _get_msgs(self):
+        # VER SI SE PUEDE UNIFICAR EN 1 CHANNEL
         connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
         channel = connection.channel()
 
@@ -34,7 +136,13 @@ class Client:
         channel.queue_bind(exchange='broadcast', queue=queue_name)
 
         def callback(ch, method, properties, body):
-            print(" R[x] Received %r" % body)
+            message_received_string = body.decode("utf-8")
+            message_received = json.loads(message_received_string)
+
+            user = message_received["username"]
+            text = message_received["message"]
+
+            print("[{}]: {}".format(user, text))
         
         channel.basic_consume(
             queue=queue_name, on_message_callback=callback, auto_ack=True)
@@ -46,19 +154,27 @@ class Client:
 
     def send(self, msg):
         if msg != '':
-            client_producer = self.username
-            
+            now = datetime.now()
+            timestamp = datetime.timestamp(now)
+
+            message = {
+                'type': "message",
+                'id': str(uuid.uuid4()),
+                'username': self.username,
+                'message': msg,
+                'timestamp': timestamp
+                }
+
+            body_message = json.dumps(message)
+
             self.channel.basic_publish(
                 exchange='',
                 routing_key='server_pending_messages',
-                body=msg,
+                body=body_message,
                 properties = pika.BasicProperties(content_type='text/plain', delivery_mode=2)
             )
-                
-            print(" [x] Sent 'Hello World!'")
-            #self.connection.close()
+            
 
-    
     #PENDIENTE
     def get_users(self):
         print("Lista de usuarios conectados: ")
@@ -97,7 +213,21 @@ class Client:
 if __name__ == '__main__':
     logging.basicConfig()
     c = Client()
-    c.get_msgs()
+
+    print("1) Conectarse")
+    print("2) Registrarse")
+    user_input = input("Escoja una opción: ")
+
+    if user_input == '1':
+        c.login()
+        c.get_msgs()
+
+    elif user_input == '2':
+        c.register()
+        c.get_msgs()
+
+    else:
+        print("Opción inválida")
 
     # Comandos de usuario
     while True:
--- Actividad2/server/server.py
+++ Actividad2/server/server.py
@@ -1,15 +1,19 @@
 from concurrent import futures
+from datetime import datetime
 import logging
 
 import pika
 import os
 import time
-
+import json
 import sys
+import uuid
+
 
 time.sleep(5)
 chats = []
-users = []
+users = {}
+logged_users = []
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters('rabbitmq'))
@@ -22,17 +26,92 @@ channel.exchange_declare(exchange='user_channel', exchange_type='direct')
 channel.exchange_declare(exchange='broadcast', exchange_type='fanout')
 
 def on_request(ch, method, props, body):
-    #response = "[USER ID]: {}".format(body)
-    response = "ok"
-    channel.basic_publish(
-        exchange='broadcast', 
-        routing_key='', 
-        body=response
-    )
+    user_message_string = body.decode("utf-8")
+    user_message_json = json.loads(user_message_string)
 
-    f = open("log.txt", "a+")
-    f.write(str(body))
-    f.close()
+    request_type = user_message_json["type"]
+    username = user_message_json["username"]
+
+    if request_type == "register":
+        password = user_message_json["password"]
+        client_uuid = str(user_message_json["uuid"])
+
+        if username not in users.keys():
+            users[username] = password
+   
+            response_message = {
+                'type': "register-confirm",
+                'response': "ok",
+                'uuid': client_uuid
+            }
+
+            channel.basic_publish(
+                exchange='user_channel', 
+                routing_key=client_uuid, 
+                body="ok")
+
+            logged_users.append(username)
+
+        else:
+            channel.basic_publish(
+                exchange='user_channel', 
+                routing_key=client_uuid, 
+                body="not ok")
+
+    elif request_type == "login":
+        password = user_message_json["password"]
+        client_uuid = str(user_message_json["uuid"])
+
+        if username not in users.keys() or username in logged_users:
+            response = "not ok"
+
+        else:
+
+            if users[username] == password:
+                response = "ok"
+                logged_users.append(username)
+            
+            else:
+                response = "not ok"
+
+        now = datetime.now()
+        timestamp = datetime.timestamp(now)
+
+        server_response = {
+            'type': "login_confirm",
+            'id': str(uuid.uuid4()),
+            'response': response,
+            'timestamp': timestamp
+        }
+
+        body_response = json.dumps(server_response)
+
+        channel.basic_publish(
+            exchange='user_channel', 
+            routing_key=client_uuid, 
+            body=body_response)
+
+    elif request_type == "message":
+        channel.basic_publish(
+            exchange='broadcast', 
+            routing_key='', 
+            body=body
+        )
+
+        """
+        f = open("log.txt", "a+")
+        f.write(str(body))
+        f.close()
+        """
+        
+    elif request_type == "users_list":
+        pass
+
+    elif request_type == "user_messages_list":
+        pass
+
+    else:
+        pass
 
     channel.basic_ack(delivery_tag=method.delivery_tag)
 
